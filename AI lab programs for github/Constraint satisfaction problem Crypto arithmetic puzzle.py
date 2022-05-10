9c1f0e6a62e6e80f3b928207e77240664c7deb79
Adithya Reddy P <90237151+AdithyaReddyP@users.noreply.github.com>
2022-05-11 01:35:24 +0530

    Add files via upload

---
+++ AI lab programs for github/Constraint satisfaction problem Crypto arithmetic puzzle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def solutions():
+    all_solutions = list()
+    for g in range(9, -1, -1):
+        for o in range(9, -1, -1):
+            for t in range(9, -1, -1):
+                for u in range(9, -1, -1):
+                    if len(set([g, o, t, u])) == 4:
+                        go = 10 * g + o
+                        to = 10 * t + o
+                        out = 100 * o + 10 * u + t
+                        if go + to == out:
+                            all_solutions.append((go, to, out))
+    return all_solutions
+print(solutions())
+
+
+# In[ ]:
+
+
+
+
